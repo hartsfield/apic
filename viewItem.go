7de6397ff92465ce292db26fe10b83e60acded4f
John Hartsfield <john@unilink.local>
2024-12-25 03:33:28 -0500

    updates

--- viewItem.go
+++ viewItem.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
-	"fmt"
 )
 
 func viewItem(w http.ResponseWriter, r *http.Request) {
 	readDB()
-id := strings.Split(r.RequestURI, "/")[2]
+	id := strings.Split(r.RequestURI, "/")[2]
 	fmt.Println(r.RequestURI, id, itemsMap[id], itemsMap)
 	exeTmpl(w, r, &viewData{AppName: itemsMap[id].Message, Stream: []*item{itemsMap[id]}}, "main.html")
 }
